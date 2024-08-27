bbe261ecdda815818a5f7e333713124bea05d455
Jeferson Rodrigues <jefersonroddl@gmail.com>
2024-08-26 22:12:49 -0300

    first commit

---
+++ album/src/components/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Searchbar = () => {
+
+  const categorias = [
+    "Natureza",
+    "Pessoas",
+    "Tecnologia",
+    "Animais",
+    "Esportes"
+  ]
+
+
+  return <div className='search-bar'>
+    <input type="text" placeholder='Pesquisar fotos...'/>
+    <button>Pesquisar</button>
+    <select>
+      {categorias.map((categoria) => (
+        <option key={categoria} value={categoria}>{categoria}</option>
+      ))}
+    </select>
+  </div>
+}
+
+export default Searchbar
